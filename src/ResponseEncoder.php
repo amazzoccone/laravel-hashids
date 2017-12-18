d25443eef73320af2679d26fbe83f33be2821a8d
Andres Mazzo <amazzoccone@bondacom.com>
2017-12-18 17:01:09 -0300

    Fix bug in ResponseEncoder

--- src/ResponseEncoder.php
+++ src/ResponseEncoder.php
@@ -10,11 +10,18 @@ namespace Bondacom\LaravelHashids;
 class ResponseEncoder
 {
     /**
+     * @var \Bondacom\LaravelHashids\Converter
+     */
+    private $converter;
+
+    /**
      * @param $response
      * @return mixed
      */
     public function handle($response)
     {
+        $this->converter = app(Converter::class);
+
         $content = json_decode($response->getContent(), true);
         $encodedContent = json_encode($this->converter->encode($content));
         $response->setContent($encodedContent);
