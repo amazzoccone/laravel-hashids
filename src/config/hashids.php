6e00c089ee62522a39267b2bcc2328505eca94a0
Andres Mazzo <amazzoccone@bondacom.com>
2017-12-20 09:14:11 -0300

    Add customizations and default params to config file

--- src/config/hashids.php
+++ src/config/hashids.php
@@ -1,14 +1,44 @@
 <?php
 
 return [
-    'salt' => env('HASHIDS_SALT', 'Bondacom'),
-    'length' => 12,
+    'system' => [
+        'salt' => env('HASHIDS_SALT', 'Bondacom'),
+        'length' => 12,
+        'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    ],
 
     /*
      * Configuration to check if parameters should be decode/encode
      * */
-    'keys' => [
+    'default' => [
         'whitelist' => ['id'],
-        'blacklist' => []
+        'blacklist' => [],
+        'separators' => ['_', '-']
+    ],
+
+    'customizations' => [
+        /*
+        * Custom configuration for request header (override default config)
+        * */
+        'header' => [
+            'whitelist' => ['id'],
+            'blacklist' => []
+        ],
+
+        /*
+         * Custom configuration for request route parameters (override default config)
+         * */
+        'route_parameters' => [
+            'whitelist' => ['id'],
+            'blacklist' => []
+        ],
+
+        /*
+         * Custom configuration for request query parameters (override default config)
+         * */
+        'query_parameters' => [
+            'whitelist' => ['id'],
+            'blacklist' => []
+        ]
     ]
 ];
