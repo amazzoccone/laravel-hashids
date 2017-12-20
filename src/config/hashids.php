803614c77c6e70302c5725999e577b8aa924cb4d
Andres Mazzo <amazzoccone@bondacom.com>
2017-12-20 17:49:42 -0300

    Fix headers config name

--- src/config/hashids.php
+++ src/config/hashids.php
@@ -25,7 +25,7 @@ return [
         /*
         * Request header
         * */
-        'header' => [
+        'headers' => [
             'whitelist' => ['id'],
             'blacklist' => []
         ],
